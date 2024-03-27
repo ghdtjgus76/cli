15b495d98392a622ab347b04e306b77123acf20a
ghdtjgus76 <ghdtjgus76@naver.com>
2024-03-28 01:23:51 +0900

    feat: init 명령어 초기 세팅

---
+++ packages/cli/src/commands/init.js
@@ -0,0 +1,13 @@
+import { Command } from "commander";
+
+const projectDependencies = ["@pandacss/dev"];
+
+export const init = new Command()
+  .name("init")
+  .description("initialize your project and install dependencies")
+  .option(
+    "-c, --cwd <cwd>",
+    "the working directory. defaults to the current directory.",
+    process.cwd()
+  )
+  .action();
