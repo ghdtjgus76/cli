1847854fd5fa6d418858386c9ee26c20dc0744ea
ghdtjgus76 <ghdtjgus76@naver.com>
2024-03-27 19:22:14 +0900

    feat: add 명령어 실행 시 저장할 디렉토리 질문하는 로직 추가

--- packages/cli/src/commands/cli.js
+++ packages/cli/src/commands/cli.js
@@ -1,13 +1,25 @@
 import { Command } from "commander";
+import readline from 'readline';
 
 const program = new Command();
+
+const getDirectory = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question("type your folder path to add your component ", (answer) => {
+    console.log(answer);
+  })
+}
 
 export const add = program
   .name("add")
   .description("add a component to your project")
   .argument("[components...]", "the components to add")
   .action(() => {
-    console.log(1);
+    getDirectory();
   });
 
 program.parse();
