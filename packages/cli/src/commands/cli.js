3ab3329c44654e81f53046409777dfcde4d3f1b6
ghdtjgus76 <ghdtjgus76@naver.com>
2024-03-27 23:17:53 +0900

    refactor: writeFileWithContent 유틸 함수로 분리

--- packages/cli/src/commands/cli.js
+++ packages/cli/src/commands/cli.js
@@ -35,6 +35,7 @@ export const add = program
       if (targetComponentInfo) {
         const file = targetComponentInfo.files[0];
         const dir = `${path}/${file.dir}`;
+        const fileContent = file.content;
         const filePath = `${path}/${file.dir}/${file.name}`;
         const dependencies = targetComponentInfo.dependencies;
 
@@ -47,25 +48,11 @@ export const add = program
               return;
             }
 
-            writeFile(filePath, file.content, (error) => {
-              if (error) {
-                console.error(`Error writing file ${filePath}:`, error);
-              } else {
-                console.log(`File ${filePath} has been written.`);
-              }
-            });
-
+            writeFileWithContent(filePath, fileContent);
             installDependencies(packageManager, dependencies, cwd);
           });
         } else {
-          writeFile(filePath, file.content, (error) => {
-            if (error) {
-              console.error(`Error writing file ${filePath}:`, error);
-            } else {
-              console.log(`File ${filePath} has been written.`);
-            }
-          });
-
+          writeFileWithContent(filePath, fileContent);
           installDependencies(packageManager, dependencies, cwd);
         }
       }
---
+++ packages/cli/src/utils/writeFileWithContent.js
@@ -0,0 +1,9 @@
+export const writeFileWithContent = (filePath, fileContent) => {
+  writeFile(filePath, fileContent, (error) => {
+    if (error) {
+      console.error(`Error writing file ${filePath}:`, error);
+    } else {
+      console.log(`File ${filePath} has been written.`);
+    }
+  });
+};
