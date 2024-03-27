cef004f115b3a0382cd6f72aebed473f68a16fff
ghdtjgus76 <ghdtjgus76@naver.com>
2024-03-27 19:54:28 +0900

    chore: 디렉토리 관련 질문은 option으로 처리하도록 변경

--- packages/cli/src/commands/cli.js
+++ packages/cli/src/commands/cli.js
@@ -1,34 +1,23 @@
 import { Command } from "commander";
-import readline from "readline";
 import { existsSync } from "fs";
 
 const program = new Command();
-
-const getDirectory = () => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  rl.question("the path to add the component to ", (answer) => {
-    // 디렉토리 존재 여부 체크
-    if (existsSync(answer)) {
-      console.log(`디렉토리: ${answer}`);
-    } else {
-      console.error(`The path ${answer} does not exist. Please try again.`);
-      process.exit(1);
-    }
-
-    rl.close();
-  });
-};
 
 export const add = program
   .name("add")
   .description("add a component to your project")
   .argument("[components...]", "the components to add")
-  .action(() => {
-    getDirectory();
+  .requiredOption("-p, --path <path>", "the path to add the component to.")
+  .action(async (components, opts) => {
+    const path = opts.path;
+
+    // 디렉토리가 존재하지 않는다면 에러 발생, 종료
+    if (!existsSync(path)) {
+      console.error(`The path ${path} does not exist. Please try again.`);
+      process.exit(1);
+    } 
+
+    console.log(path);
   });
 
 program.parse();
