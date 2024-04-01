d0ac50fbf8f9929d2bfefbdab35d245455c75f94
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-01 23:14:32 +0900

    feat: 현재 디렉토리부터 가장 가까운 package.json 파일을 찾는 함수 및 로직 init에 추가

--- packages/cli/src/commands/init.js
+++ packages/cli/src/commands/init.js
@@ -1,4 +1,8 @@
 import { Command } from "commander";
+import { existsSync } from "fs";
+import path from "path";
+import { z } from "zod";
+import { getNearestPackageJson } from "../utils/getNearestPackageJson.js";
 
 const projectDependencies = ["@pandacss/dev"];
 
@@ -6,7 +10,9 @@ const initOptionSchema = z.object({
   cwd: z.string(),
 });
 
-export const init = new Command()
+const program = new Command();
+
+export const init = program
   .name("init")
   .description("initialize your project and install dependencies")
   .option(
@@ -17,8 +23,22 @@ export const init = new Command()
   .action((opts) => {
     const options = initOptionSchema.parse(opts);
 
+    const cwd = path.resolve(options.cwd);
+
     if (!existsSync(cwd)) {
       console.error(`The path ${cwd} does not exist. Please try again.`);
       process.exit(1);
     }
+
+    const packageJsonPath = getNearestPackageJson(cwd);
+
+    if (packageJsonPath) {
+      console.log(packageJsonPath);
+    } else {
+      console.error(
+        "node_modules or package.json not found in the current directory or its parent directories"
+      );
+    }
   });
+
+program.parse();
---
+++ packages/cli/src/utils/getNearestPackageJson.js
@@ -0,0 +1,24 @@
+import path from "path";
+import { existsSync } from "fs";
+
+export const getNearestPackageJson = (cwd) => {
+  let currentDir = cwd;
+
+  while (true) {
+    const packageJsonPath = path.join(currentDir, "package.json");
+
+    if (existsSync(packageJsonPath)) {
+      return packageJsonPath;
+    }
+
+    const parentDir = path.resolve(currentDir, "..");
+
+    if (parentDir === currentDir) {
+      break;
+    }
+
+    currentDir = parentDir;
+  }
+
+  return null;
+};
