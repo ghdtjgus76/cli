bd0792ae8867be89133400db5ec1122da980fb76
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-15 23:03:27 +0900

    feat: 다른 파일 의존성 모듈 이름 가져오기 기능 추가

--- packages/cli/src/commands/deployRegistry.js
+++ packages/cli/src/commands/deployRegistry.js
@@ -1,13 +1,25 @@
 import { Command } from "commander";
 import { promises as fs } from "fs";
+import { Project } from "ts-morph";
 
 const program = new Command();
 
+// TODO 1: 해당 컴포넌트 의존성 불러오기
+// TODO 2: 컴포넌트 파일 내용 불러오기
 export const deployRegistry = program
   .name("deployRegistry")
   .description("deploy component registry file")
   .argument("component", "the component to deploy registry")
   .action(async (component) => {
+    const project = new Project();
+    const sourceFile = project.addSourceFileAtPath(
+      `/Users/ghdtjgus/Desktop/design-system-cli/design-system-cli/packages/ui/components/${component}.tsx`
+    );
+
+    sourceFile.getImportDeclarations().forEach((importDeclaration) => {
+      console.log(importDeclaration.getModuleSpecifier().getLiteralValue());
+    });
+
     const filePath = `${component}.json`;
 
     const fileContent = {
