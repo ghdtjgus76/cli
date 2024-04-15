ae36625f87d2276f96a59bb4196ad6b36b75aff1
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-15 21:04:25 +0900

    feat: 파일 내용 생성 및 stringify 기능 추가

--- packages/cli/src/commands/deployRegistry.js
+++ packages/cli/src/commands/deployRegistry.js
@@ -1,4 +1,5 @@
 import { Command } from "commander";
+import { promises as fs } from "fs";
 
 const program = new Command();
 
@@ -6,4 +7,22 @@ export const deployRegistry = program
   .name("deployRegistry")
   .description("deploy component registry file")
   .argument("component", "the component to deploy registry")
-  .action();
+  .action(async (component) => {
+    const filePath = `/registry/${component}.json`;
+
+    const fileContent = {
+      name: `${component}`,
+      dependencies: ["dependency1"],
+      files: [
+        {
+          name: `${component}.tsx`,
+          content: "content",
+        },
+      ],
+    };
+
+    const stringifiedFileContent = JSON.stringify(fileContent);
+    console.log(stringifiedFileContent);
+  });
+
+program.parse();
