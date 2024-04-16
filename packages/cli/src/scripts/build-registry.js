d746e11e38709ee8f50bfd8791a771b74bef636e
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-16 16:50:40 +0900

    feat: 레지스트리 파일 저장할 디렉토리 기능 추가

--- packages/cli/src/scripts/build-registry.js
+++ packages/cli/src/scripts/build-registry.js
@@ -3,7 +3,7 @@ import { promises as fs } from "fs";
 import { Project } from "ts-morph";
 import { filterExistingPath } from "../utils/filterExistingPath.js";
 
-const buildRegistry = async (component, componentPath) => {
+const buildRegistry = async (component, componentPath, registryPath) => {
   const absoluteComponentPath = path.resolve(componentPath);
 
   const project = new Project();
@@ -19,7 +19,7 @@ const buildRegistry = async (component, componentPath) => {
     }
   });
 
-  const filePath = `${component}.json`;
+  const filePath = `${registryPath}/${component}.json`;
 
   const fileContent = {
     name: `${component}`,
@@ -36,7 +36,9 @@ const buildRegistry = async (component, componentPath) => {
   await fs.writeFile(filePath, stringifiedFileContent);
 };
 
-buildRegistry(process.argv[2], process.argv[3]).catch((error) => {
-  console.error("An error occurred:", error);
-  process.exit(1);
-});
+buildRegistry(process.argv[2], process.argv[3], process.argv[4]).catch(
+  (error) => {
+    console.error("An error occurred:", error);
+    process.exit(1);
+  }
+);
