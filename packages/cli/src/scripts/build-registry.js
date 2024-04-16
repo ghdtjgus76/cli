e1b3657e4cb96591109e8fc55d20d6c3b2498ded
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-16 16:38:59 +0900

    feat: build registry 파일 추가 (기능은 registry cli와 동일)

---
+++ packages/cli/src/scripts/build-registry.js
@@ -0,0 +1,42 @@
+import path from "path";
+import { promises as fs } from "fs";
+import { Project } from "ts-morph";
+import { filterExistingPath } from "../utils/filterExistingPath.js";
+
+const buildRegistry = async (component, componentPath) => {
+  const absoluteComponentPath = path.resolve(componentPath);
+
+  const project = new Project();
+  const sourceFile = project.addSourceFileAtPath(absoluteComponentPath);
+
+  const dependencies = [];
+
+  sourceFile.getImportDeclarations().forEach((importDeclaration) => {
+    const module = importDeclaration.getModuleSpecifier().getLiteralValue();
+
+    if (filterExistingPath(module)) {
+      dependencies.push(module);
+    }
+  });
+
+  const filePath = `${component}.json`;
+
+  const fileContent = {
+    name: `${component}`,
+    dependencies,
+    files: [
+      {
+        name: `${component}.tsx`,
+        content: sourceFile.getText(),
+      },
+    ],
+  };
+  const stringifiedFileContent = JSON.stringify(fileContent);
+
+  await fs.writeFile(filePath, stringifiedFileContent);
+};
+
+buildRegistry(process.argv[2], process.argv[3]).catch((error) => {
+  console.error("An error occurred:", error);
+  process.exit(1);
+});
