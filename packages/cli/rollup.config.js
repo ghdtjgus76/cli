56876f7deaaedd2c1def1e0d527b0ffb4a3ff293
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-22 21:24:57 +0900

    fix: 빌드 파일 생성 디렉토리 문제 해결

--- packages/cli/rollup.config.js
+++ packages/cli/rollup.config.js
@@ -1,8 +1,8 @@
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import * as pkg from "./package.json" assert { type: "json" };
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"]; // 어떤 확장자를 처리 할 지 정함
 
@@ -12,17 +12,15 @@ process.env.BABEL_ENV = "production";
 export default {
   input: "./src/index.ts", // 어떤 파일부터 불러올지 정함.
   plugins: [
-    peerDepsExternal() /* peerDependencies로 설치한 라이브러리들을 external 모듈로 설정
-                               즉, 번들링된 결과에 포함시키지 않음 */,
-    resolve({ extensions }), // node_modules 에서 모듈을 불러올 수 있게 해줌. ts/tsx 파일도 불러올 수 있게 해줌
-    commonjs({
-      include: "node_modules/**",
-    }), // CommonJS 형태로 만들어진 모듈도 불러와서 사용 할 수 있게 해줌. 현재 프로젝트 상황에서는 없어도 무방함
+    peerDepsExternal(), // peerDependencies로 설치한 라이브러리들을 external 모듈로 설정
+    resolve({ extensions, preferBuiltins: true }), // node_modules 에서 모듈을 불러올 수 있게 해줌. ts/tsx 파일도 불러올 수 있게 해줌
+    commonjs(), // CommonJS 형태로 만들어진 모듈도 불러와서 사용 할 수 있게 해줌. 현재 프로젝트 상황에서는 없어도 무방함
     babel({ extensions, include: ["src/**/*"], runtimeHelpers: true }), // Babel을 사용 할 수 있게 해줌
+    json(),
   ],
   output: [
     {
-      file: pkg.module, // 번들링한 파일을 저장 할 경로
+      file: "./dist/index.js", // 번들링한 파일을 저장 할 경로
       format: "es", // ES Module 형태로 번들링함
     },
   ],
